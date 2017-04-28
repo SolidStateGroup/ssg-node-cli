ed7b0f5c1c0d9fc0ce3b1070e68f342695c181f7
kylejohnson <kyle@solidstategroup.com>
2017-04-28 07:44:52 +0100

    Execute server on install, remove unused dependencies

--- bin/index.js
+++ bin/index.js
@@ -2,12 +2,11 @@
 
 var fs = require('fs');
 var path = require('path');
-var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
 
 var options = require('minimist')(process.argv.slice(2));
 var commands = options._;
-var spinner = require("char-spinner")
+var spinner = require("char-spinner");
 var PROJECT_NAME = commands[0];
 
 
@@ -20,19 +19,14 @@ if (options.f) {
 
 execSync(`git clone https://github.com/kyle-ssg/node-minimal ./${PROJECT_NAME}`, { stdio: 'inherit' });
 
-//install web
-var web = new Promise((resolve) => {
 
-});
-//install mobile
-
-
-console.log('Installing web and mobile, this should take around 10-20 seconds...')
+console.log('Installing dependencies, this should take around 10-20 seconds...')
 var interval = spinner(options);
 execSync(`cd ./${PROJECT_NAME} && rm -rf .git && git init`, { stdio: 'inherit' });
 execSync(`cd ./${PROJECT_NAME} && npm i`, { stdio: 'inherit' });
+execSync(`cd ./${PROJECT_NAME} && npm run dev`, { stdio: 'inherit' });
 
 
-console.log('All done, enjoy your day.')
+console.log('All done, enjoy your day.');
 clearInterval(interval);
 

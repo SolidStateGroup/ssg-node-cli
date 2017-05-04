6ad3e87d7f9590673d3a6de3e449ae6b8b8cf7dd
kylejohnson <kyle@solidstategroup.com>
2017-05-04 11:33:14 +0100

    open webstorm via cli

--- bin/index.js
+++ bin/index.js
@@ -22,6 +22,12 @@ execSync(`git clone https://github.com/kyle-ssg/node-minimal ./${PROJECT_NAME}`,
 
 console.log('Installing dependencies, this should take around 10-20 seconds...')
 var interval = spinner(options);
+try {
+    execSync(`open -a /Applications/WebStorm.app ./${PROJECT_NAME}`, { stdio: 'inherit' });
+} catch (e) {
+    console.log("Add ws to your alias", e);
+}
+
 execSync(`cd ./${PROJECT_NAME} && rm -rf .git && git init`, { stdio: 'inherit' });
 execSync(`cd ./${PROJECT_NAME} && npm i`, { stdio: 'inherit' });
 execSync(`cd ./${PROJECT_NAME} && npm run dev`, { stdio: 'inherit' });
